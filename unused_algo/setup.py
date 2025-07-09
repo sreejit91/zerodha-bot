98c1c8c614d33f3a12171465b5d25850bc6d3edb
sreejit91 <sreejit.chandra@gmail.com>
2025-07-09 17:00:52 +0530

    BRN-08: feat: Removed unused algo and fixed data_manager, broker (#8)

--- unused_algo/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="algo",          # package name that ‘pip install’ will register
-    version="0.1.0",
-    packages=find_packages(),   # automatically includes your algo/ folder
-)
